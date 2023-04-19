5a58bdd703cd2246b07ccca0255d28dcc5d5b1b1
Andrey Lobachev <andrey_lobachev@internet.ru>
2023-04-19 12:01:12 +0300

    Main.java updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,21 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvValidationException;
-
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
-
         List<Employee> list = parseCSV(columnMapping, fileName);
-        parseCSV(columnMapping, fileName);
-
-        System.out.println(list);
+        String json = listToJson(list);
+        writeString(json);
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -37,5 +35,19 @@ public class Main {
         return null;
     }
 
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        return gson.toJson(list);
+    }
 
+    public static void writeString(String s) {
+        try (FileOutputStream fos = new FileOutputStream("data.json")) {
+            byte[] bytes = s.getBytes();
+            fos.write(bytes, 0, bytes.length);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
